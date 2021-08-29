fdabe06955618e43de73f28cd46e0cd7ba6082b5
SoftwareSing <softwaresing@gmail.com>
2021-08-29 20:46:20 +0800

    let home page show all waifu list

--- src/HomePage.svelte
+++ src/HomePage.svelte
@@ -1,15 +1,17 @@
 <script>
   import { onDestroy } from 'svelte'
-  import { navigate } from 'svelte-routing'
+  import { Link } from 'svelte-routing'
   import { getWaifuList, syncServerWaifuEvent } from './waifu/WaifuManager'
+  import { formatNumber } from './utils/formatter'
+
+  let waifuList = []
 
   const unsubscribeSyncEvent = syncServerWaifuEvent.subscribe(() => {
-    const waifuList = getWaifuList()
-    if (waifuList.length < 1) return
+    const newList = getWaifuList()
+    if (newList.length < 1) return
 
-    waifuList.sort((a, b) => b.popCount - a.popCount)
-    const firstWaifu = waifuList[0]
-    navigate(`/${firstWaifu.urlId}`, { replace: true })
+    newList.sort((a, b) => b.popCount - a.popCount)
+    waifuList = newList
   })
 
   onDestroy(() => {
@@ -17,11 +19,33 @@
   })
 </script>
 
-<div class="position-relative vh-100 vw-100">
-  <div class="position-absolute top-50 start-50 translate-middle">
-    loading...
+<div class="container">
+  <div class="d-flex align-content-center flex-wrap">
+    {#each waifuList as waifu }
+      <Link to="/{waifu.urlId}" class="mx-auto mt-3 text-dark" style="text-decoration: none;">
+        <div class="card waifu-card">
+          <img class="card-img-top mx-auto" src="{waifu.imgNormalUrl}" alt="{waifu.name} image" />
+          <div class="card-body">
+            <h5 class="card-title">{waifu.name}</h5>
+            <div class="card-text">
+              <div class="text-end">{formatNumber(waifu.popCount)}</div>
+            </div>
+          </div>
+        </div>
+      </Link>
+    {/each}
   </div>
 </div>
 
 <style>
+  .waifu-card {
+    width: 250px;
+  }
+
+  .waifu-card img {
+    object-fit: cover;
+    object-position: center;
+    height: 248px;
+    width: 248px;
+  }
 </style>
