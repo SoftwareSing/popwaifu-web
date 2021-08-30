1e91c5b97c5ba02718eec9892cf48439e789dab7
SoftwareSing <softwaresing@gmail.com>
2021-08-30 21:58:45 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const reloadWaifuTime = 15 * 1000
-export const reloadPopCountTime = 60
+export const reloadWaifuTime = 5 * 1000
+export const reloadPopCountTime = 65
 export const defaultModeName = 'default'
 
 export const uploadPopLimit = 500
