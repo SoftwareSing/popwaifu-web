90c1835ffdef50b0dfa45f2ca6059261052d1e4b
SoftwareSing <softwaresing@gmail.com>
2021-09-02 22:13:33 +0800

    change upload pop time

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ export const reloadWaifuTime = 5 * 1000
 export const reloadPopCountTime = 65
 export const defaultModeName = 'default'
 
-export const uploadPopLimit = 500
-export const intervalUploadPopTime = 15 * 1000
+export const uploadPopLimit = 150
+export const intervalUploadPopTime = 5 * 1000
 
 export const exampleWaifuData = {
   waifuId: 'exampleId',
